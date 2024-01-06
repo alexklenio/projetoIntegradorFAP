2e89dbdabd093d0e5fb3b64b58f7202603400b62
Jorge Braga <jorgeabbraga@gmail.com>
2024-01-06 06:19:27 -0300

    Atualização de Autorização Token

--- cangame/src/app/routes/routes.ts
+++ cangame/src/app/routes/routes.ts
@@ -12,11 +12,11 @@ import { createLicenca, deleteLicenca, getLicencaById, getLicencas, updateLicenc
 const routers = Router();
 
 // Rotas do Cadastro e Consultas dos Usuários
-routers.get('/users', getUsers); //http://localhost:3001/users
-routers.get('/user/:id', getUserById);//http://localhost:3001/user/id
-routers.post('/user', createUser);//http://localhost:3001/user
-routers.put('/user/:id', updateUser);//http://localhost:3001/user/id
-routers.delete('/user/:id', deleteUser);//http://localhost:3001/user/id
+routers.get('/users', authenticationMiddleware, getUsers); //http://localhost:3001/users
+routers.get('/user/:id', authenticationMiddleware, getUserById);//http://localhost:3001/user/id
+routers.post('/user', authenticationMiddleware, createUser);//http://localhost:3001/user
+routers.put('/user/:id', authenticationMiddleware, updateUser);//http://localhost:3001/user/id
+routers.delete('/user/:id', authenticationMiddleware, deleteUser);//http://localhost:3001/user/id
 
 //Rota para fazer o Login do Usuário
 routers.post('/login', loginUser);//http://localhost:3001/login
@@ -24,47 +24,47 @@ routers.post('/login', loginUser);//http://localhost:3001/login
 
 //Rotas do Cadastro e Consultas de Administrador
 routers.get('/administradores', authenticationMiddleware, getAdministradores);//http://localhost:3001/administradores
-routers.get('/administrador/:id', getAdministradorById);//http://localhost:3001/administrador/id
-routers.post('/administrador', createAdministrador);//http://localhost:3001/administrador
-routers.put('/administrador/:id', updateAdministrador); //http://localhost:3001/administrador/id
-routers.delete('/administrador/:id', deleteAdministrador);//http://localhost:3001/administrador/id
+routers.get('/administrador/:id', authenticationMiddleware, getAdministradorById);//http://localhost:3001/administrador/id
+routers.post('/administrador', authenticationMiddleware, createAdministrador);//http://localhost:3001/administrador
+routers.put('/administrador/:id', authenticationMiddleware, updateAdministrador); //http://localhost:3001/administrador/id
+routers.delete('/administrador/:id', authenticationMiddleware, deleteAdministrador);//http://localhost:3001/administrador/id
 
 //Rotas do Cadastro e Consultas de Contrato
 routers.get('/contratos', authenticationMiddleware, getContratos);//http://localhost:3001/contratos
-routers.get('/contrato/:id', getContratoById);//http://localhost:3001/contrato/id
-routers.post('/contrato', createContrato);//http://localhost:3001/contrato
-routers.put('/contrato/:id', updateContrato);//http://localhost:3001/contrato/id
-routers.delete('/contrato/:id', deleteContrato);//http://localhost:3001/contrato
+routers.get('/contrato/:id', authenticationMiddleware, getContratoById);//http://localhost:3001/contrato/id
+routers.post('/contrato', authenticationMiddleware, createContrato);//http://localhost:3001/contrato
+routers.put('/contrato/:id', authenticationMiddleware, updateContrato);//http://localhost:3001/contrato/id
+routers.delete('/contrato/:id', authenticationMiddleware, deleteContrato);//http://localhost:3001/contrato
 
 
 //Rotas do Cadastro e Consultas de Licenças
 routers.get('/licencas', authenticationMiddleware, getLicencas);//http://localhost:3001/licencas
-routers.get('/licenca/:id', getLicencaById);//http://localhost:3001/licenca/id
-routers.post('/licenca', createLicenca);//http://localhost:3001/licenca
-routers.put('/licenca/:id', updateLicenca);//http://localhost:3001/licenca/id
-routers.delete('/licenca/:id', deleteLicenca);//http://localhost:3001/licenca/id
+routers.get('/licenca/:id', authenticationMiddleware, getLicencaById);//http://localhost:3001/licenca/id
+routers.post('/licenca', authenticationMiddleware, createLicenca);//http://localhost:3001/licenca
+routers.put('/licenca/:id', authenticationMiddleware, updateLicenca);//http://localhost:3001/licenca/id
+routers.delete('/licenca/:id', authenticationMiddleware, deleteLicenca);//http://localhost:3001/licenca/id
 
 //Rotas do Cadastro e Consultas de Gestor
 routers.get('/gestores', authenticationMiddleware, getGestores);//http://localhost:3001/gestores
-routers.get('/gestor/:id', getGestorById);//http://localhost:3001/gestor/id
-routers.post('/gestor', createGestor);//http://localhost:3001/gestor
-routers.put('/gestor/:id', updateGestor);//http://localhost:3001/gestor/id
-routers.delete('/gestor/:id', deleteGestor);//http://localhost:3001/gestor/id
+routers.get('/gestor/:id', authenticationMiddleware, getGestorById);//http://localhost:3001/gestor/id
+routers.post('/gestor', authenticationMiddleware, createGestor);//http://localhost:3001/gestor
+routers.put('/gestor/:id', authenticationMiddleware, updateGestor);//http://localhost:3001/gestor/id
+routers.delete('/gestor/:id', authenticationMiddleware, deleteGestor);//http://localhost:3001/gestor/id
 
 //Rotas do Cadastro e Consultas de Endereço
 routers.get('/enderecos', authenticationMiddleware, getEnderecos);//http://localhost:3001/enderecos
-routers.get('/endereco/:id', getEnderecoById);//http://localhost:3001/endereco/id
-routers.post('/endereco', createEndereco);//http://localhost:3001/endereco
-routers.put('/endereco/:id', updateEndereco);//http://localhost:3001/endereco/id
-routers.delete('/endereco/:id', deleteEndereco);//http://localhost:3001/endereco/id
+routers.get('/endereco/:id', authenticationMiddleware, getEnderecoById);//http://localhost:3001/endereco/id
+routers.post('/endereco', authenticationMiddleware, createEndereco);//http://localhost:3001/endereco
+routers.put('/endereco/:id', authenticationMiddleware, updateEndereco);//http://localhost:3001/endereco/id
+routers.delete('/endereco/:id', authenticationMiddleware, deleteEndereco);//http://localhost:3001/endereco/id
 
 
 //Rotas do Cadastro e Consultas de Telefone
 routers.get('/telefones', authenticationMiddleware, getTelefones);//http://localhost:3001/telefones
-routers.get('/telefone/:id', getTelefoneById);//http://localhost:3001/telefone/id
-routers.post('/telefone', createTelefone);//http://localhost:3001/telefone
-routers.put('/telefone/:id', updateTelefone);//http://localhost:3001/telefone/id
-routers.delete('/telefone/:id', deleteTelefone);//http://localhost:3001/telefone/id
+routers.get('/telefone/:id', authenticationMiddleware, getTelefoneById);//http://localhost:3001/telefone/id
+routers.post('/telefone', authenticationMiddleware, createTelefone);//http://localhost:3001/telefone
+routers.put('/telefone/:id', authenticationMiddleware, updateTelefone);//http://localhost:3001/telefone/id
+routers.delete('/telefone/:id', authenticationMiddleware, deleteTelefone);//http://localhost:3001/telefone/id
 
 export default routers;
 
--- cangame/src/app/services/AuthService.ts
+++ cangame/src/app/services/AuthService.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import  IUser  from '../interfaces/IUser';
 
 const generateToken = (user: IUser): string => {
-    const token = jwt.sign({ id: user.id, email: user.email }, 'cangame123', { expiresIn: '1h' });
+    const token = jwt.sign({ id: user.id, email: user.email }, 'cangame123', { expiresIn: '8h' });
     return token;
 };
 
